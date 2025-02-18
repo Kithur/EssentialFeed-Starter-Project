3404d1647aa6ed2a6ecdf82eb1a28beb89d4b37f
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-02-18 10:46:32 -0600

    Add memory leaks tracking for components in integration

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -33,19 +33,20 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func getFeedResult() -> LoadFeedResult? {
-            let testServerURL = URL(string: "http://essentialdeveloper.com/feed-case-study/test-api/feed")!
-            let client = URLSessionHTTPClient()
-            let loader = RemoteFeedLoader(url: testServerURL, client: client)
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
+        let testServerURL = URL(string: "http://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        let exp = expectation(description: "Wait for load completion")
 
-            let exp = expectation(description: "Wait for load completion")
-
-            var receivedResult: LoadFeedResult?
-            loader.load { result in
-                receivedResult = result
-                exp.fulfill()
-            }
-            wait(for: [exp], timeout: 5.0)
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
         return receivedResult
     }
 
