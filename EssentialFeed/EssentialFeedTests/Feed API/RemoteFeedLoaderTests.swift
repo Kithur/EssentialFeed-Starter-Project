ea6e36fc3eb822613dc55e0e743d26207bea131a
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-04-21 11:09:27 -0600

    Replace initializer

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -60,7 +60,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
 
         expect(sut, toCompleteWith: failure(.invalidData), when: {
-            let invalidJSON = Data(_: "invalid json".utf8)
+            let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
     }
