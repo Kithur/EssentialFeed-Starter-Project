9b44e6065f5cf01dcbfafe08c194926b3c4ddd8f
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-06-18 09:09:41 -0600

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Luis Roberto Gutiérrez Carbajal on 17/06/25.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
