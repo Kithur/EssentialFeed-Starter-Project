c39fc637d6085900e65395ec89033dd1a2bf8ec3
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-08-06 10:00:20 -0600

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Luis Roberto Gutiérrez Carbajal on 06/08/25.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
