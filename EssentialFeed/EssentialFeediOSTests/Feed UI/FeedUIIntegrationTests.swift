912dda89c0128319979fb1ec804c65d9bfc43738
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-08-19 10:23:43 -0600

    Extract 'UIButton' helper to its own file

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedUIIntegrationTests.swift
@@ -392,16 +392,6 @@ private extension FeedImageCell {
     }
 }
 
-private extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
-
 private extension FeedViewController {
     func replaceRefreshControlWithFakeForiOSSupport() {
         let fake = FakeRefreshControl()
