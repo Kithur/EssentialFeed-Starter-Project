0e6bebd40fd7f8e4cecf2c1711f99015adcac5e0
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-09-08 10:15:57 -0600

    Move 'FeedLoadingViewModel' to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Luis Roberto Gutiérrez Carbajal on 08/09/25.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
