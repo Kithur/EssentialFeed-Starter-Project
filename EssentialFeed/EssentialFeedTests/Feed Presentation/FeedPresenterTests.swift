fedadccb5b0605c65c43ef08dd4e825e965053ca
Luis Roberto Gutiérrez Carbajal <lrobgc.kp@gmail.com>
2025-09-08 09:27:58 -0600

    'FeedPresenter' does not send messages to view on init

---
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -0,0 +1,31 @@
+//
+//  FeedPresenterTests.swift
+//  EssentialFeed
+//
+//  Created by Luis Roberto Gutiérrez Carbajal on 08/09/25.
+//
+
+import XCTest
+
+final class FeedPresenter {
+    init(view: Any) {
+
+    }
+}
+
+class FeedPresenterTests: XCTestCase {
+
+    func test_init_doesNotSendMesssagesToView() {
+        let view = ViewSpy()
+
+        _ = FeedPresenter(view: view)
+
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+
+    // MARK: - Helpers
+
+    private class ViewSpy {
+        let messages = [Any]()
+    }
+}
